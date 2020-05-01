a21d409bde7656b44854b09b6eb07e93639e814b
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-05-01 08:53:45 +0300

    HW4: Reflection

---
+++ src/ru/topjava/basejava/MainReflection.java
@@ -0,0 +1,14 @@
+package ru.topjava.basejava;
+
+import ru.topjava.basejava.model.Resume;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Resume resume = new Resume();
+        Method method = resume.getClass().getDeclaredMethod("toString");
+        System.out.println(method.invoke(resume));
+    }
+}
