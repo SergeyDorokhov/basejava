0ecbbe8e46a0031bfe4792b0086e1de49fce6817
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-05-20 21:50:02 +0300

    HW8. Bypass folders, DateUtil

---
+++ src/ru/topjava/basejava/util/Bypass.java
@@ -0,0 +1,31 @@
+package ru.topjava.basejava.util;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Bypass {
+    public static void findAllFiles(String path) throws IOException {
+        File startFolder = new File(path);
+        File[] files = startFolder.listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                printFileName(file);
+            } else {
+                findAllFiles(file.getPath());
+            }
+        }
+    }
+
+    private static void printFileName(File file) throws IOException {
+        System.out.println(file.getCanonicalPath());
+    }
+
+    public static void main(String[] args) {
+        String startFolder = "C:\\Projects\\java\\basejava";
+        try {
+            findAllFiles(startFolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
