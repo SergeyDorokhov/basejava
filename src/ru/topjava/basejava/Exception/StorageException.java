cfce34a2af62c3b51d1c98e2e22b6142d1f06c7a
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-05-22 23:22:58 +0300

    HW8: add AbstractFileStorage

--- src/ru/topjava/basejava/Exception/StorageException.java
+++ src/ru/topjava/basejava/Exception/StorageException.java
@@ -8,6 +8,11 @@ public class StorageException extends RuntimeException {
         this.uuid = uuid;
     }
 
+    public StorageException(String message, String uuid, Exception exception) {
+        super(message, exception);
+        this.uuid = uuid;
+    }
+
     public String getUuid() {
         return uuid;
     }
---
+++ src/ru/topjava/basejava/storage/AbstractFileStorage.java
@@ -0,0 +1,87 @@
+package ru.topjava.basejava.storage;
+
+import ru.topjava.basejava.Exception.StorageException;
+import ru.topjava.basejava.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private final File storage;
+
+    public AbstractFileStorage(File storage) {
+        Objects.requireNonNull(storage, "storage must not be null");
+        if (!storage.isDirectory()) {
+            throw new IllegalArgumentException(storage.getAbsolutePath() + " is not directory");
+        }
+        if (!storage.canRead() || !storage.canWrite()) {
+            throw new IllegalArgumentException(storage.getAbsolutePath() + " is not readable/writeable");
+        }
+        this.storage = storage;
+    }
+
+    @Override
+    protected File getPointer(String uuid) {
+        return new File(storage, uuid);
+    }
+
+    @Override
+    protected boolean isExist(File pointer) {
+        return pointer.exists();
+    }
+
+    @Override
+    protected void addToStorage(Resume resume, File pointer) {
+        try {
+            if (pointer.createNewFile()) {
+                doWrite(resume, pointer);
+            }
+        } catch (IOException e) {
+            throw new StorageException("IO error", pointer.getName(), e);
+        }
+    }
+
+    @Override
+    protected Resume getFromStorage(File pointer) {
+        return doRead(pointer);
+    }
+
+    @Override
+    protected void updateStorage(Resume resume, File pointer) {
+        doWrite(resume, pointer);
+    }
+
+    @Override
+    protected void deleteFromStorage(File pointer) {
+        pointer.delete();
+    }
+
+    @Override
+    protected List<Resume> getList() {
+        File[] files = storage.listFiles();
+        Resume[] resumes = new Resume[files.length];
+        for (int i = 0; i < resumes.length; i++) {
+            resumes[i] = doRead(files[i]);
+        }
+        return Arrays.asList(resumes);
+    }
+
+    @Override
+    public void clear() {
+        for (File file : storage.listFiles()) {
+            file.delete();
+        }
+    }
+
+    @Override
+    public int size() {
+        return storage.listFiles().length;
+    }
+
+    protected abstract void doWrite(Resume resume, File pointer);
+
+    protected abstract Resume doRead(File pointer);
+}
