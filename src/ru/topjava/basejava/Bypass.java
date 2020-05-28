195daa478483599dfa21d48ccd32aee0e219e530
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-05-28 08:29:49 +0300

    Lesson9: output with offset

--- src/ru/topjava/basejava/Bypass.java
+++ src/ru/topjava/basejava/Bypass.java
@@ -2,28 +2,30 @@ package ru.topjava.basejava;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Bypass {
-    public static void findAllFiles(String path) throws IOException {
+    public static void findAllFiles(String offset, String path) throws IOException {
         File startFolder = new File(path);
         File[] files = startFolder.listFiles();
-        for (File file : files) {
+        for (File file : Objects.requireNonNull(files)) {
             if (file.isFile()) {
-                printFileName(file);
+                printFileName(offset + "file: ", file);
             } else {
-                findAllFiles(file.getPath());
+                printFileName(offset + "dir: ", file);
+                findAllFiles(offset + " ", file.getPath());
             }
         }
     }
 
-    private static void printFileName(File file) throws IOException {
-        System.out.println(file.getCanonicalPath());
+    private static void printFileName(String offset, File file) throws IOException {
+        System.out.println(offset + file.getName());
     }
 
     public static void main(String[] args) {
-        String startFolder = "C:\\Projects\\java\\basejava";
+        String startFolder = "C:\\Projects\\java\\basejava\\src\\ru\\topjava\\basejava";
         try {
-            findAllFiles(startFolder);
+            findAllFiles("", startFolder);
         } catch (IOException e) {
             e.printStackTrace();
         }
