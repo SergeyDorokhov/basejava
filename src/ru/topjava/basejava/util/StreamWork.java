42bc5f0c0383050c28c46a7372e8b8d89b9ab35e
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-06-18 19:26:33 +0300

    HW12. StreamWork fix

--- src/ru/topjava/basejava/util/StreamWork.java
+++ src/ru/topjava/basejava/util/StreamWork.java
@@ -2,25 +2,23 @@ package ru.topjava.basejava.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StreamWork {
 
     public static int minValue(int[] values) {
-        int[] sortedValues = Arrays.stream(values).distinct().sorted().toArray();
-        int sum = 0;
-        int degree = sortedValues.length - 1;
-        int multiplier;
-        for (int value : sortedValues) {
-            multiplier = (int) Math.pow(10, degree--);
-            sum += value * multiplier;
-        }
-        return sum;
+        return Arrays.stream(values).distinct().sorted().
+                reduce((s1, s2) -> Integer.parseInt(s1 + "" + s2)).getAsInt();
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
         return (integers.stream().mapToInt(s -> s).sum() % 2 == 0)
-                ? integers.stream().filter(s -> s % 2 != 0).collect(Collectors.toList())
-                : integers.stream().filter(s -> s % 2 == 0).collect(Collectors.toList());
+                ? getCollect(integers, s -> s % 2 != 0)
+                : getCollect(integers, s -> s % 2 == 0);
+    }
+
+    private static List<Integer> getCollect(List<Integer> integers, Predicate<? super Integer> predicate) {
+        return integers.stream().filter(predicate).collect(Collectors.toList());
     }
 }
