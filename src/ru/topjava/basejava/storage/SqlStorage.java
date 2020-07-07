5a1b47469fcac1f1bd2219b4983002b0de331981
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-07-07 10:40:03 +0300

    HW15 fix: rewrite getAllSorted

--- src/ru/topjava/basejava/storage/SqlStorage.java
+++ src/ru/topjava/basejava/storage/SqlStorage.java
@@ -11,22 +11,19 @@ import java.util.logging.Logger;
 
 public class SqlStorage implements Storage {
     private final SqlHelper helper;
-    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+    private static final Logger log = Logger.getLogger(AbstractStorage.class.getName());
     private static final String INSERT_RESUME = "INSERT INTO resume (uuid, full_name) VALUES (?,?)";
     private static final String SELECT_RESUME = "SELECT * FROM resume r LEFT JOIN contact c " +
             "ON r.uuid = c.resume_uuid LEFT JOIN section s ON r.uuid = s.resume_uuid WHERE r.uuid = ?";
-    private static final String SELECT_RESUMES = "SELECT * FROM resume ORDER BY full_name, uuid";
+    private static final String SELECT_EMPTY_RESUME = "SELECT * FROM resume ORDER BY full_name, uuid";
     private static final String DELETE_RESUME = "DELETE FROM resume WHERE uuid =?";
     private static final String DELETE_RESUMES = "DELETE FROM resume";
     private static final String UPDATE_RESUME = "UPDATE resume SET full_name = ? WHERE uuid=?";
     private static final String COUNT_RESUMES = "SELECT COUNT(*) from resume";
-    private static final String SELECT_CONTACT = "SELECT * FROM contact WHERE resume_uuid = ?";
+    private static final String SELECT_CONTACTS = "SELECT * FROM contact";
     private static final String DELETE_CONTACTS = "DELETE FROM contact where resume_uuid =?";
-    private static final String SELECT_SECTION = "SELECT * FROM section WHERE resume_uuid = ?";
+    private static final String SELECT_SECTIONS = "SELECT * FROM section";
     private static final String DELETE_SECTIONS = "DELETE FROM section where resume_uuid =?";
-    private static final String SELECT_RESUMES_WITH_ATTRIBUTES = "SELECT * FROM resume r LEFT JOIN contact c " +
-            "ON r.uuid = c.resume_uuid LEFT JOIN section s " +
-            "ON r.uuid = s.resume_uuid ORDER BY full_name, uuid";
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
         try {
@@ -47,7 +44,7 @@ public class SqlStorage implements Storage {
 
     @Override
     public void save(Resume resume) {
-        LOG.info("Save " + resume);
+        log.info("Save " + resume);
         helper.executeTransaction(conn -> {
             try (PreparedStatement ps = conn.prepareStatement(INSERT_RESUME)) {
                 helper.setParam(ps, resume.getUuid(), resume.getFullName());
@@ -61,11 +58,11 @@ public class SqlStorage implements Storage {
 
     @Override
     public Resume get(String uuid) {
-        LOG.info("Get " + uuid);
+        log.info("Get " + uuid);
         return helper.connectAndQuery(SELECT_RESUME, ps -> {
             try (ResultSet result = helper.setParam(ps, uuid).executeQuery()) {
                 if (!result.next()) {
-                    LOG.warning("Resume " + uuid + " not exist");
+                    log.warning("Resume " + uuid + " not exist");
                     throw new NotExistStorageException(uuid);
                 }
                 Resume resume = new Resume(uuid, result.getString("full_name"));
@@ -80,7 +77,7 @@ public class SqlStorage implements Storage {
 
     @Override
     public void update(Resume resume) {
-        LOG.info("Update " + resume);
+        log.info("Update " + resume);
         helper.executeTransaction(conn -> {
             try (PreparedStatement ps = conn.prepareStatement(UPDATE_RESUME)) {
                 helper.setParam(ps, resume.getFullName(), resume.getUuid());
@@ -96,7 +93,7 @@ public class SqlStorage implements Storage {
 
     @Override
     public void delete(String uuid) {
-        LOG.info("Delete " + uuid);
+        log.info("Delete " + uuid);
         helper.connectAndQuery(DELETE_RESUME, ps -> {
             helper.executeStatement(helper.setParam(ps, uuid), uuid);
             return null;
@@ -105,19 +102,18 @@ public class SqlStorage implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        Map<String, Resume> map = new LinkedHashMap<>();
         return helper.executeTransaction(conn -> {
-            Map<String, Resume> map = new LinkedHashMap<>();
-            try (PreparedStatement ps = conn.prepareStatement(SELECT_RESUMES_WITH_ATTRIBUTES)) {
+            try (PreparedStatement ps = conn.prepareStatement(SELECT_EMPTY_RESUME)) {
                 try (ResultSet res = ps.executeQuery()) {
                     while (res.next()) {
                         String uuid = res.getString("uuid").trim();
-                        String name = res.getString("full_name");
-                        Resume resume = map.computeIfAbsent(uuid, k -> new Resume(uuid, name));
-                        addContact(res, resume);
-                        addSection(res, resume);
+                        map.put(uuid, new Resume(uuid, res.getString("full_name")));
                     }
                 }
             }
+            addAttributes(conn, map, SELECT_CONTACTS);
+            addAttributes(conn, map, SELECT_SECTIONS);
             return new ArrayList<>(map.values());
         });
     }
@@ -133,7 +129,7 @@ public class SqlStorage implements Storage {
 
     public void addContact(ResultSet result, Resume resume) throws SQLException {
         String contact = result.getString("value");
-        if (contact != null && !resume.getContacts().containsValue(contact)) {
+        if (contact != null) {
             ContactType contactType = ContactType.valueOf(result.getString("type"));
             resume.addContact(contactType, contact);
         }
@@ -184,11 +180,8 @@ public class SqlStorage implements Storage {
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
                         List<String> records = ((ListSection) entry.getValue()).getData();
-                        StringBuilder builder = new StringBuilder();
-                        for (String record : records) {
-                            builder.append(record).append("\n");
-                        }
-                        helper.setParam(ps, builder.toString(), entry.getKey().name(), resume.getUuid());
+                        String record = String.join("\n", records);
+                        helper.setParam(ps, record, entry.getKey().name(), resume.getUuid());
                         ps.addBatch();
                         break;
                     default:
@@ -196,6 +189,26 @@ public class SqlStorage implements Storage {
                 }
             }
             ps.executeBatch();
+        }
+    }
+
+    private void addAttributes(Connection conn, Map<String, Resume> map, String sql) throws SQLException {
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            try (ResultSet res = ps.executeQuery()) {
+                while (res.next()) {
+                    Resume resume = map.get(res.getString("resume_uuid").trim());
+                    switch (sql) {
+                        case SELECT_CONTACTS:
+                            addContact(res, resume);
+                            break;
+                        case SELECT_SECTIONS:
+                            addSection(res, resume);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
     }
 
